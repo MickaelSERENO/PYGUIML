f573afea7ac221a4a80902a61efc2a73428765e8
Gaulois94 <serenomickael@gmail.com>
2013-05-05 19:12:41 +0200

    Module who contain generators for updates and draw functions

---
+++ src/decorator.py
@@ -0,0 +1,21 @@
+from Widget import Widget
+
+def forDrawing(function):
+	def returnForDrawing(self, render=None, *args, **kwargs):
+		if self.isDrawing:
+			if not render:
+				render = self.getRender()
+			return funcion(self, render, *args, **kwargs)
+
+		return Widget.draw(self, render, *args, **kwargs)
+	return returnForDrawing
+
+def forUpdate(function):
+	def returnForUpdate(self, render=None, *args, **kwargs):
+		if self.canUpdate:
+			if not render:
+				render = self.getRender()
+			return funcion(self, render, *args, **kwargs)
+
+		return Widget.update(self, render, *args, **kwargs)
+	return returnForUpdate
