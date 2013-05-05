890ae0a52b4e0c0cbbe94e40f689ad3466706408
Gaulois94 <serenomickael@gmail.com>
2013-05-05 19:11:44 +0200

    The Button is now on the beta

--- src/Button.py
+++ src/Button.py
@@ -1,175 +1,198 @@
 from Widget import Widget
+from Widget import Position
 from Label import Label
 from Image import Image
+from Active import Active
+import decorator
 import sfml as sf
 
-class Button(Widget):
-	"""This Widget is a Button. It regrouped a guiml.Image and guiml.Label. With it, you can use a real Button."""
-	def __init__(self, parent, text=Label(), image=Image(), rect=sf.FloatRect(0,0,0,0)):
+class Button(Widget, Active):
+	"""This Widget is a Button. It regrouped a guiml.Image and guiml.Label.
+	With it, you can use a real Button."""
+	def __init__(self, parent, text=None, image=None, rect=sf.Rectangle()):
 		Widget.__init__(self, parent, rect)
+		Active.__init__(self)
 		
-		self._text = text.getCopyWidget()
-		self._textLighten = text.getCopyWidget()
-		self._image = image.getCopyWidget()
-		self._imageLighten = image.getCopyWidget()
-
-		self._imageLighten.lighten()
-		self._textLighten.lighten()
-
-		self._currentImage = self._image
+		self._text = text
+		self._textLighten = text
 		self._currentText = self._text
 
-		if self._image.dimensions.x != 0 and self._image.dimensions.y != 0 :
-			self._hasImage = True
-		else:
-			self._hasImage = False
+		self._image = image
+		self._imageLighten = image
+		self._currentImage = self._image
+		self._useCharacterSize = None
 
-		if self._text.dimensions.y != 0 and self._text.dimensions.x != 0 :
-			self._hasText = True
-		else:
-			self._hasText = False
+		if image:
+			self._imageLighten.lighten()
+			self._image.canFocus = self._imageLighten.canFocus = False
+			self._image.isStaticToView =\
+					self._imageLighten.isStaticToView = False
+		if text:
+			self._textLighten.lighten()
+			self._image.canFocus = self._imageLighten.canFocus = False
+			self._text.isStaticToView =\
+					self._textLighten.isStaticToView = False
 
-		if rect.width != 0 and rect.height != 0 :
+		if rect == sf.Rectangle() and not(self._image or self._text):
 			self.rect = rect
 
 		elif self._hasImage:
 			self.rect = self._image.rect
 
-		elif self._hasText:
+		elif self._hasLabel:
 			self.rect = self._text.rect
 
-		self._isSelect = False
-		self._isActive = False
-		self._isSelectCopy = False
-		self._isActiveCopy = False
+		self.howActiveKeyboard = sf.Keyboard.SPACE
+		self.howActiveMouse = sf.Mouse.LEFT
+		self.drawWidget(True)
 
-		self._howActiveKeyboard = sf.Keyboard.ESCAPE
-		self._howActiveMouse = sf.Mouse.LEFT
+	@decorator.forUpdate
+	def update(self, render=None):
+		Active.update(self)
+		Widget.update(self, render)
 
-	def update(self, drawable):
-		if self._isDrawing:
-			self.cursorInButton()
-
-			if self._isSelect and (self.getEvent().getOneMouseClicked(self._howActiveMouse) or self.getEvent().getOnePressedKeys(self._howActiveKeyboard)):
-				self._isActive = True
-			else:
-				self._isActive = False
-
-		Widget.update(self, drawable)
+	@decorator.forDrawing
+	def draw(self, render=None):
+		if render:
+			render.draw(self._currentImage)
+			render.draw(self._currentText)
 
 	def centerLabel(self):
 		"""Set The text at the button's middle"""
+		if self._text:
+			self._text.posOrigin = Position.Center
+			self._textLighten = Position.Center
 
-		self._text.setOriginMiddle()
-		self._text._setPos = sf.Vector2f(self.pos.x + self.dimensions.x / 2, self.pos.y + self.dimensions.y / 2)
-		self._textLighten.setOriginMiddle()
-		self._textLighten.pos = sf.Vector2f(self.pos.x + self.dimensions.x / 2, self.pos.y + self.dimensions.y / 2)
+	def updateSelection(self):
+		self._isSelect = Widget.widgetFocus is self
+		return Active.updateSelection(self)
 
-	def cursorInButton(self):
-		"""See if the Cursor is in the Button or no. After, it lightUp the Button if the Button is select"""
+	def selectIt(self):
+		self.lightUpDrawable(True)
 
-		if self.getEvent().isMouseInRect(self.rect) or self._isSelectCopy:
-			self._isSelect = True
-			self.lightUpDrawable()
-		else:
-			self._isSelect = False
-			self.lightUpDrawable(False)
+	def deselectIt(self):
+		self.lightUpDrawable(False)
+
+	def updateActive(self):
+		self._isActive = self.isSelect and self.event and \
+				(self.event.getOneMouseClicked(self.howActiveMouse) or\
+				self.event.getOnePressedKeys(self.howActiveKeyboard))
+		return Active.updateSelection(self)
 
 	def lightUpDrawable(self, lighten = True):
 		"""Light Up the Button if lighten is True value"""
 
 		if lighten:
-			if self._hasImage:
+			if self._image:
 				self._imageLighten.parent = self
 				self._image.parent = 0
 				self._currentImage = self._imageLighten
 				
-			if self._hasText:
+			if self._text:
 				self._textLighten.parent = self
 				self._text.parent = 0
 				self._currentText = self._textLighten
 
 		else:
-			if self._hasImage:
+			if self._image:
 				self._imageLighten.parent = 0
 				self._image.parent = self
 				self._currentImage = self._image
 
-			if self._hasText:
+			if self._text:
 				self._textLighten.parent = 0
 				self._text.parent = self
 				self._currentText = self._text
 
-	def _setDimensions(self, dimensions):
-		self._image.dimensions = dimensions
-		self._imageLighten.dimensions = dimensions
-		self._text.setTextWidthSize(dimensions.x)
-		self._textLighten.setTextWidthSize(dimensions.x)
-		Widget._setDimensions(self, dimensions)
-		self.centerLabel()
+	def drawWidget(self, draw=True):
+		if self._text:
+			self._text.isDrawing = True
+			self._textLighten.isDrawing = True
+		if self._image:
+			self._image.isDrawing = True
+			self._imageLighten.isDrawing = True
+		Widget.drawWidget(self, draw)
 
-	def _setPos(self, pos):
-		self._image.pos = pos
-		self._imageLighten.pos = pos
-		self._text.pos = pos
-		self._textLighten.pos = pos
-		Widget._setPos(self, pos)
-		self.centerLabel()
-		print(pos.x, pos.y)
+	def setPos(self, pos, withOrigin=True):
+		Widget.setPos(self, pos, withOrigin)
 
-	def _getIsDrawing(self):
-		return self.__isDrawing
+		if self.hasImage:
+			self._image.pos = self.pos
+			self._imageLighten.pos = self.pos
+		if self.hasLabel:
+			self._text.pos = self.pos + self.size/2
+			self._textLighten.pos = self.pos + self.size/2
+
+	def _setSize(self, size):
+		Widget._setSize(self, size)
+
+		if self.hasImage:
+			self._image.size = self.virtualSize
+			self._imageLighten.size = self.virtualSize
+
+		if self.hasLabel and not self.useCharacterSize:
+			self._text.setTextWidthSize(self.virtualSize.x)
+			self._textLighten.setTextWidthSize(self.virtualSize.x)
+
+			if self.virtualSize.y > 0 and \
+					self._text.virtualSize.y > self.virtualSize.y :
+				self._text.setTextHeightSize(self.virtualSize.y)
+				self._textLighten.setTextHeightSize(self.virtyalSize.y)
 
 	def _drawWidget(drawing):
 		self._text._isDrawing = drawing
 		self._textLighten._isDrawing = drawing
 		self._image._isDrawing = drawing
 		self._imageLighten._isDrawing = drawing
-		self._isDrawing = drawing
-
-	def _getImage(self):
-		return self._image
+		self.isDrawing = drawing
 
 	def _setImage(self, image):
 		"""image is a Image type"""
 
-		self._image = image.getCopyWidget()
-		self._imageLighten = image.getCopyWidget()
+		self._image = image
+		self._imageLighten = image
 		self._imageLighten.lighten()
 
-		if image.dimensions.x != 0 and image.dimensions.y != 0:
-			self._hasImage = True
-			self.rect = self.rect
+		if image:
+			self._image.canFocus = self._imageLighten.canFocus = False
+			self._image.isStaticToView = \
+					self._imageLighten.isStaticToView = False
 
-		else:
-			self._hasImage = False
-
-	def _getText(self):
-		return self._text
-
+		self.rect = self.rect
 	def _setText(self, text):
 		"""text is Label type"""
 
 		self._text = text.getCopyWidget()
 		self._textLighten = text.getCopyWidget()
 		self._textLighten.lighten()
+		if text:
+			self._text.canFocus = self._textLighten.canFocus = False
+			self._text.isStaticToView = \
+					self._textLighten.isStaticToView = False
 
-		if text.dimensions.x != O and text.dimensions.y != 0:
-			self._hasText = True
-			self.rect = self.rect
+		self.rect = self.rect
+		self.updateSelection()
+		self.centerLabel()
 
-		else:
-			self._hasText = False
+	def _setUseCharacterSize(self, use=True):
+		self._useCharacterSize = use
+	
+	def _setCharacterSize(self, size):
+		if self.hasLabel:
+			self._text.characterSize = size
+			self._textLighten.characterSize = size
+		self.useCharacterSize = True
 
-	def _getHasLabel(self):
-		return self._hasLabel
+	image = property(lambda self:self._currentImage, _setImage)
+	text = property(lambda self:self._currentText, _setText)
+	basicText = property(lambda self:self._text)
+	basicImage = property(lambda self:self._image)
+	textLighten = property(lambda self:self._textLighten)
+	imageLighten= property(lambda self:self._imageLighten)
+	hasLabel = property(lambda self:bool(self._text))
+	hasImage = property(lambda self:bool(self._image))
+	characterSize =property(lambda self:self._text.characterSize,\
+			_setCharacterSize)
+	useCharacterSize = property(lambda self:self._useCharacterSize,\
+			_setUseCharacterSize)
 
-	def _getHasImage(self):
-		return self._hasImage
-
-	image = property(_getImage, _setImage)
-	text = property(_getText, _setText)
-	hasLabel = property(_getHasLabel)
-	hasImage = property(_getHasImage)
-	pos = property(Widget._getPos, _setPos)
-	dimensions = property(Widget._getDimensions, _setDimensions)
