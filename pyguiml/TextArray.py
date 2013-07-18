fd9c2e347425b2cd87796361a4e85b9029dbb159
Gaulois94 <serenomickael@gmail.com>
2013-07-18 22:36:24 +0200

    Add text alignment

--- pyguiml/TextArray.py
+++ pyguiml/TextArray.py
@@ -2,18 +2,24 @@ from Label import Label
 from Widget import *
 import sfml as sf
 from functools import reduce
+import decorator
 
 class TextArray(Widget):
 	def __init__(self, parent = None, pos=sf.Vector2(0, 0), sizeX=200,\
-			space=5, label=Label()):
+			space=5, label=Label(), alignment = Position.TopLeft):
 		Widget.__init__(self, parent,sf.Rectangle(pos, sf.Vector2(sizeX, 0)))
 
 		self._labelList = list()
 		self._space = space
 		self._label = None
+		self._alignment = None
 
 		self.setLabel(label, False)
+		self.alignment = alignment
 
+	@decorator.forUpdate
+	def update(self, render=None):
+		Widget.update(self, render)
 
 	def setPos(self, pos, withOrigin=True):
 		oldPos = self.pos
@@ -22,6 +28,10 @@ class TextArray(Widget):
 			label.pos = reduce(lambda x, y : x + sf.Vector2(0, y.size.y),\
 							self._labelList[0:i], self._pos) + \
 							sf.Vector2(0, self._space * i)
+	def drawWidget(self, draw=True):
+		for label in seld._labelList:
+			label.drawWidget(draw)
+		Widget.drawWidget(self, draw)
 
 	def setSize(self, size):
 		Widget.setSize(self, size)
@@ -39,11 +49,9 @@ class TextArray(Widget):
 
 		oldCharacterIndex = characterIndex
 		oldCharacterPos = characterPos
-		print("len", len(label.text.string))
 		while oldCharacterIndex < len(label.text.string):
 			for i in range(oldCharacterIndex, len(label.text.string)):
 				characterIndex += 1
-				print(characterIndex)
 				if label.text.find_character_pos(i).x - oldCharacterPos > self.size.x\
 						or label.text.string[i] == '\n' or characterIndex > len(label.text.string):
 
@@ -52,15 +60,15 @@ class TextArray(Widget):
 
 					characterPos = label.text.find_character_pos(characterIndex).x
 
-					if label.text.string[oldCharacterIndex:characterIndex] != '\n':
+					#if label.text.string[oldCharacterIndex:characterIndex] != '\n':
 
-						self._labelList.append(Label(self, \
-							label.text.string[oldCharacterIndex:characterIndex].replace('\n', ''),\
-							reduce(lambda x, y : x + sf.Vector2(0, y.size.y),\
-							self._labelList, self._pos) + \
-							sf.Vector2(0, self._space * len(self._labelList)),\
-							label.characterSize, label.font,\
-							label.style))
+					self._labelList.append(Label(self, \
+						label.text.string[oldCharacterIndex:characterIndex].replace('\n', ''),\
+						reduce(lambda x, y : x + sf.Vector2(0, y.size.y),\
+						self._labelList, self._pos) + \
+						sf.Vector2(0, self._space * len(self._labelList)),\
+						label.characterSize, label.font,\
+						label.style))
 
 #						self._labelList[-1].size = sf.Vector2(characterPos - oldCharacterPos, \
 #								self._labelList[-1].size.y)
@@ -73,5 +81,26 @@ class TextArray(Widget):
 			Widget.setSize(self, sf.Vector2(max([newLabel.size.x for newLabel in self._labelList]+[0]), \
 				self._labelList[-1].getPos(False).y + self._labelList[-1].size.y - self._pos.y))
 		self._label = label
+		self.alignment = self.alignment
+
+	def _setAlignment(self, alignment):
+		self._alignment = alignment
+		for label in self._labelList:
+			if label:
+				if alignment == Position.TopLeft or alignment==Position.BottomLeft:
+					label.origin = sf.Vector2(0,0)
+				elif alignment == Position.TopRight or alignment==Position.BottomRight:
+					label.origin = sf.Vector2(self.size.x, 0) - \
+							sf.Vector2(label.size.x, 0)
+				else:
+					label.origin = sf.Vector2(self.size.x - label.size.x, 0)/2
+
+		self.setPos(self.getPos(False), False)
+
+	def setCanUpdate(self, update):
+		for label in self._labelList:
+			label.canUpdate = update
+		Updatable.setCanUpdate(self, update)
 
 	label = property(lambda self:self._label, setLabel)
+	alignment = property(lambda self:self._alignment, _setAlignment)
